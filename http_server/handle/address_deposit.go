9dc12d0b172e51139b0234852fc1e31dcfe831d7
scorpiotzh <835598264@qq.com>
2022-04-18 17:23:54 +0800

    feat: edit address deposit

--- http_server/handle/address_deposit.go
+++ http_server/handle/address_deposit.go
@@ -1,35 +1,24 @@
 package handle
 
 import (
+	"das_register_server/config"
 	"das_register_server/http_server/api_code"
-	"encoding/json"
+	"fmt"
+	"github.com/DeAccountSystems/das-lib/common"
 	"github.com/gin-gonic/gin"
+	"github.com/nervosnetwork/ckb-sdk-go/address"
 	"github.com/scorpiotzh/toolib"
 	"net/http"
+	"regexp"
 )
 
 type ReqAddressDeposit struct {
+	AlgorithmId common.DasAlgorithmId `json:"algorithm_id"`
+	Address     string                `json:"address"`
 }
 
 type RespAddressDeposit struct {
-}
-
-func (h *HttpHandle) RpcAddressDeposit(p json.RawMessage, apiResp *api_code.ApiResp) {
-	var req []ReqAddressDeposit
-	err := json.Unmarshal(p, &req)
-	if err != nil {
-		log.Error("json.Unmarshal err:", err.Error())
-		apiResp.ApiRespErr(api_code.ApiCodeParamsInvalid, "params invalid")
-		return
-	} else if len(req) == 0 {
-		log.Error("len(req) is 0")
-		apiResp.ApiRespErr(api_code.ApiCodeParamsInvalid, "params invalid")
-		return
-	}
-
-	if err = h.doAddressDeposit(&req[0], apiResp); err != nil {
-		log.Error("doAddressDeposit err:", err.Error())
-	}
+	CkbAddress string `json:"ckb_address"`
 }
 
 func (h *HttpHandle) AddressDeposit(ctx *gin.Context) {
@@ -58,6 +47,59 @@ func (h *HttpHandle) AddressDeposit(ctx *gin.Context) {
 
 func (h *HttpHandle) doAddressDeposit(req *ReqAddressDeposit, apiResp *api_code.ApiResp) error {
 	var resp RespAddressDeposit
+
+	chainType := common.ChainTypeEth
+	is712 := false
+	switch req.AlgorithmId {
+	case common.DasAlgorithmIdEth:
+		if ok, _ := regexp.MatchString("^0x[0-9a-fA-F]{40}$", req.Address); ok {
+			apiResp.ApiRespErr(api_code.ApiCodeError500, "address invalid")
+			return nil
+		}
+		chainType = common.ChainTypeEth
+	case common.DasAlgorithmIdEth712:
+		if ok, _ := regexp.MatchString("^0x[0-9a-fA-F]{40}$", req.Address); ok {
+			apiResp.ApiRespErr(api_code.ApiCodeError500, "address invalid")
+			return nil
+		}
+		chainType = common.ChainTypeEth
+		is712 = true
+	case common.DasAlgorithmIdTron:
+		if _, err := common.TronBase58ToHex(req.Address); err != nil {
+			apiResp.ApiRespErr(api_code.ApiCodeError500, "address invalid")
+			return nil
+		}
+	case common.DasAlgorithmIdEd25519:
+		if ok, _ := regexp.MatchString("^0x[0-9a-fA-F]{64}$", req.Address); ok {
+			apiResp.ApiRespErr(api_code.ApiCodeError500, "address invalid")
+			return nil
+		}
+	default:
+		apiResp.ApiRespErr(api_code.ApiCodeError500, "algorithm_id invalid")
+		return nil
+	}
+
+	lockScript, _, err := h.dasCore.FormatAddressToDasLockScript(chainType, req.Address, is712)
+	if err != nil {
+		apiResp.ApiRespErr(api_code.ApiCodeError500, err.Error())
+		return fmt.Errorf("FormatAddressToDasLockScript err: %s", err.Error())
+	}
+
+	if config.Cfg.Server.Net == common.DasNetTypeMainNet {
+		addr, err := address.ConvertScriptToAddress(address.Mainnet, lockScript)
+		if err != nil {
+			apiResp.ApiRespErr(api_code.ApiCodeError500, err.Error())
+			return fmt.Errorf("ConvertScriptToAddress err: %s", err.Error())
+		}
+		resp.CkbAddress = addr
+	} else {
+		addr, err := address.ConvertScriptToAddress(address.Testnet, lockScript)
+		if err != nil {
+			apiResp.ApiRespErr(api_code.ApiCodeError500, err.Error())
+			return fmt.Errorf("ConvertScriptToAddress err: %s", err.Error())
+		}
+		resp.CkbAddress = addr
+	}
 
 	apiResp.ApiRespOK(resp)
 	return nil
