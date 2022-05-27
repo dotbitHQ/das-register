4a9706d18d5e6516a6c75f21346691e59303b910
scorpiotzh <835598264@qq.com>
2022-05-27 19:33:48 +0800

    feat: check channel acc

--- http_server/handle/order_register.go
+++ http_server/handle/order_register.go
@@ -219,9 +219,10 @@ func (h *HttpHandle) checkOrderInfo(req *ReqOrderRegisterBase, apiResp *api_code
 		if err != nil {
 			apiResp.ApiRespErr(api_code.ApiCodeDbError, "search channel account fail")
 			return fmt.Errorf("GetAccountInfoByAccountId err: %s", err.Error())
-		} else if acc.Id == 0 {
-			apiResp.ApiRespErr(api_code.ApiCodeChannelAccountNotExist, "channel account not exist")
-			return nil
+		} else if acc.Id == 0 || acc.Status == tables.AccountStatusOnCross || acc.IsExpired() {
+			//apiResp.ApiRespErr(api_code.ApiCodeChannelAccountNotExist, "channel account not exist")
+			//return nil
+			req.ChannelAccount = ""
 		}
 	}
 	return nil
