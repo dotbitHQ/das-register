d784ba484db6a49bd2ac9352761b42425ec932cf
scorpiotzh <835598264@qq.com>
2022-06-27 15:34:54 +0800

    feat: test edit custom script

--- http_server/handle/edit_script.go
+++ http_server/handle/edit_script.go
@@ -203,7 +203,7 @@ func (h *HttpHandle) buildEditScript(req *reqBuildTx, p *editScriptParams) (*txb
 	txParams.OutputsData = append(txParams.OutputsData, subAccountOutputData)
 
 	// action witness
-	actionWitness, err := witness.GenActionDataWitnessV2(common.DasActionCreateSubAccount, nil, common.ParamOwner)
+	actionWitness, err := witness.GenActionDataWitnessV2(common.DasActionConfigSubAccountCreatingScript, nil, common.ParamOwner)
 	if err != nil {
 		return nil, fmt.Errorf("GenActionDataWitness err: %s", err.Error())
 	}
