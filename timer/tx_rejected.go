b39c2ab2885a1e95aeb65c97b1082eb133cc254c
scorpiotzh <835598264@qq.com>
2022-09-13 16:06:39 +0800

    feat: add rejected tx check

--- timer/tx_rejected.go
+++ timer/tx_rejected.go
@@ -4,35 +4,40 @@ import (
 	"das_register_server/config"
 	"das_register_server/notify"
 	"fmt"
+	"github.com/nervosnetwork/ckb-sdk-go/types"
 	"strings"
 	"time"
 )
 
 func (t *TxTimer) doTxRejected() error {
-	countRefund, err := t.dbDao.GetUnRefundTxCount()
-	if err != nil {
-		return fmt.Errorf("GetUnRefundTxCount err: %s", err.Error())
-	}
 	list, err := t.dbDao.GetMaybeRejectedRegisterTxs(time.Now().Add(-time.Minute*10).UnixNano() / 1e6)
 	if err != nil {
 		return fmt.Errorf("GetMaybeRejectedRegisterTxs err: %s", err.Error())
 	}
-	if countRefund == 0 && len(list) == 0 {
+	if len(list) == 0 {
 		return nil
 	}
-	msg := `> un refund txs: %d 
-> rejected register tx: %d
+
+	msg := `> rejected register tx: %d
 %s`
 	var orderList []string
 	for _, v := range list {
+		txRes, err := t.dasCore.Client().GetTransaction(t.ctx, types.HexToHash(v.Hash))
+		if err != nil {
+			log.Error("GetTransaction err: %s", err.Error())
+		} else {
+			log.Info("GetTransaction:", v.OrderId, v.Hash, txRes.TxStatus.Status)
+			if txRes.TxStatus.Status == types.TransactionStatusRejected {
+				if err := t.dbDao.UpdateRejectedTx(v.Action, v.OrderId); err != nil {
+					log.Error("UpdateRejectedTx err: %s", err.Error())
+				}
+			}
+		}
+		//
 		sinceMin := time.Since(time.Unix(v.Timestamp/1000, 0)).Minutes()
 		orderList = append(orderList, fmt.Sprintf("%s : %s (%.2f min)", v.Action, v.OrderId, sinceMin))
 	}
-	msg = fmt.Sprintf(msg, countRefund, len(list), strings.Join(orderList, "\n"))
-	if len(list) > 0 {
-		notify.SendLarkTextNotifyAtAll(config.Cfg.Notify.LarkErrorKey, "Rejected Txs", msg)
-	} else {
-		notify.SendLarkTextNotify(config.Cfg.Notify.LarkErrorKey, "Rejected Txs", msg)
-	}
+	msg = fmt.Sprintf(msg, len(list), strings.Join(orderList, "\n"))
+	notify.SendLarkTextNotify(config.Cfg.Notify.LarkErrorKey, "Rejected Txs", msg)
 	return nil
 }
