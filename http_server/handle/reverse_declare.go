4d7b73e8b8cbd3e87734ab3ae6a183996c701dd9
scorpiotzh <835598264@qq.com>
2022-09-13 18:12:34 +0800

    feat: fix balance fee

--- http_server/handle/reverse_declare.go
+++ http_server/handle/reverse_declare.go
@@ -233,7 +233,7 @@ func (h *HttpHandle) buildTx(req *reqBuildTx, txParams *txbuilder.BuildTransacti
 	} else if req.Action == common.DasActionTransfer {
 		sizeInBlock, _ := txBuilder.Transaction.SizeInBlock()
 		changeCapacity := txBuilder.Transaction.Outputs[len(txBuilder.Transaction.Outputs)-1].Capacity + common.OneCkb - sizeInBlock - 1000
-		txBuilder.Transaction.Outputs[1].Capacity = changeCapacity
+		txBuilder.Transaction.Outputs[len(txBuilder.Transaction.Outputs)-1].Capacity = changeCapacity
 	}
 	signList, err := txBuilder.GenerateDigestListFromTx(skipGroups)
 	if err != nil {
