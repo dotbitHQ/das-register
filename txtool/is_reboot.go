39e0a2bbff04ba9f698e8eb152b6786690ac6fa1
scorpiotzh <835598264@qq.com>
2022-10-14 10:44:54 +0800

    feat: add reboot check

--- txtool/is_reboot.go
+++ txtool/is_reboot.go
@@ -4,7 +4,7 @@ import "time"
 
 func (t *TxTool) IsRebootTxOK() bool {
 	rebootSeconds := time.Since(t.RebootTime).Seconds()
-	if rebootSeconds > 120 {
+	if rebootSeconds > 60 {
 		return true
 	}
 	log.Info("IsRebootTxOK:", rebootSeconds)
