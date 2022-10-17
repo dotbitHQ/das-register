d238b0e662f5dad7a39c2ea8d12c66a1360e5211
scorpiotzh <835598264@qq.com>
2022-10-18 00:18:20 +0800

    feat: test

--- http_server/handle/account_search.go
+++ http_server/handle/account_search.go
@@ -329,7 +329,8 @@ func (h *HttpHandle) checkAccountBase(req *ReqAccountSearch, apiResp *api_code.A
 			tcTimestamp := tc.Timestamp()
 			openTimestamp := int64(1666094400)
 			if config.Cfg.Server.Net != common.DasNetTypeMainNet {
-				openTimestamp = 1666094400
+				//openTimestamp = 1666094400
+				openTimestamp = 1665712800
 			}
 			// check dao char type
 			isSameDaoCharType := true
