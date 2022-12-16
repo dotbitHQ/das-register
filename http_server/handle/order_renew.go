cc774e6fb80a0b6f274e13b31040cc69623aef67
scorpiotzh <835598264@qq.com>
2022-12-16 19:47:59 +0800

    feat: Cannot be renewed 2 hours before the end of the grace period

--- http_server/handle/order_renew.go
+++ http_server/handle/order_renew.go
@@ -299,7 +299,7 @@ func (h *HttpHandle) checkRenewOrder(req *ReqOrderRenew, apiResp *api_code.ApiRe
 	}
 	expirationGracePeriod, _ := builder.ExpirationGracePeriod()
 	log.Info("checkRenewOrder:", expirationGracePeriod)
-	expirationGracePeriod -= 12 * 60 * 60
+	expirationGracePeriod -= 2 * 60 * 60
 	log.Info("checkRenewOrder:", expirationGracePeriod)
 	if int64(acc.ExpiredAt+uint64(expirationGracePeriod)) <= time.Now().Unix() {
 		apiResp.ApiRespErr(api_code.ApiCodeAfterGracePeriod, "after the grace period")
