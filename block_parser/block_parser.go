83e1c4949b723d9e20d7e0f3ad724e89aaef9767
scorpiotzh <835598264@qq.com>
2022-12-26 22:52:42 +0800

    feat: add checkContractVersion

--- block_parser/block_parser.go
+++ block_parser/block_parser.go
@@ -162,6 +162,9 @@ func (b *BlockParser) parserConcurrencyMode() error {
 }
 
 func (b *BlockParser) parsingBlockData(block *types.Block) error {
+	if err := b.checkContractVersion(); err != nil {
+		return err
+	}
 	for _, tx := range block.Transactions {
 		txHash := tx.Hash.Hex()
 		blockNumber := block.Header.Number
@@ -192,5 +195,36 @@ func (b *BlockParser) parsingBlockData(block *types.Block) error {
 		}
 	}
 	b.errCountHandle = 0
+	return nil
+}
+
+var contractNames = []common.DasContractName{
+	common.DasContractNameApplyRegisterCellType,
+	common.DasContractNamePreAccountCellType,
+	common.DasContractNameProposalCellType,
+	common.DasContractNameConfigCellType,
+	common.DasContractNameAccountCellType,
+	//common.DasContractNameAccountSaleCellType,
+	common.DASContractNameSubAccountCellType,
+	//common.DASContractNameOfferCellType,
+	//common.DasContractNameBalanceCellType,
+	//common.DasContractNameIncomeCellType,
+	common.DasContractNameReverseRecordCellType,
+	//common.DASContractNameEip712LibCellType,
+}
+
+func (b *BlockParser) checkContractVersion() error {
+	for _, v := range contractNames {
+		defaultVersion, chainVersion, err := b.DasCore.CheckContractVersion(v)
+		if err != nil {
+			if err == core.ErrContractMajorVersionDiff {
+				log.Error("chain version is[%s],service version is[%s],please upgrade service.", chainVersion, defaultVersion)
+				//log.Fatal("checkContractVersion:", defaultVersion, chainVersion)
+				//b.ctx.Done()
+				return err
+			}
+			return fmt.Errorf("CheckContractVersion err: %s", err.Error())
+		}
+	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module das_register_server
 go 1.16
 
 require (
-	github.com/dotbitHQ/das-lib v1.0.1-0.20221214031200-2d9c984b6490
+	github.com/dotbitHQ/das-lib v1.0.1-0.20221226074936-f2cfb943228c
 	github.com/elazarl/goproxy v0.0.0-20220417044921-416226498f94 // indirect
 	github.com/ethereum/go-ethereum v1.10.17
 	github.com/fsnotify/fsnotify v1.5.3
