e795448cc7d754753803d77cbce207e296d8d6ab
jerry <i@sjis.me>
2023-02-27 16:53:38 +0800

    feat: update test

--- http_server/handle/reverse_send_test.go
+++ http_server/handle/reverse_send_test.go
@@ -7,8 +7,10 @@ import (
 )
 
 func TestPersonalSign(t *testing.T) {
-	signature := fixSignature("0x6e6c9b3f72a47eb15ee6336e776e91aee8a2d4390f86ffa226b099482b95537a3bfd96bb663cfe0e16daa4300c1d8eb3b13a5d8fec9c97112dbafa07b52e29f11b")
-	signOk, err := sign.VerifyPersonalSignature(common.Hex2Bytes(signature), []byte("0x3636373236663664323036343639363433613230392f4e4d4c6e5957486f78325a48714b6933627044714264467554384d703148317954717537626e4542733d"), "0xc0d0087dA03480f9d7e7E1D76d2DCa4bb0A98B17")
+	digest := common.Hex2Bytes("0x66726f6d206469643a2057f2e68d6c982d02c0495307b820107448ae7ab3de841ec072b7250c90efd55e")
+	signature := common.Hex2Bytes(fixSignature("0x06b3abdf1a885d2a4741d39250a1080d66e3ba47add98c091574b1feb886a68e20e587add97c600064f0cace958671ebabe83c351f5d6265f1808d16e2ec65361c"))
+	address := "0xdeeFC10a42cD84c072f2b0e2fA99061a74A0698c"
+	signOk, err := sign.VerifyPersonalSignature(signature, digest, address)
 	if err != nil {
 		t.Fatal(err)
 	}
