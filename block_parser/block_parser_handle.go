3134ae5c3fb405507170c3dbb1e23a4a357b9dd8
jerry <i@sjis.me>
2023-03-01 17:03:36 +0800

    feat: update reverse_smt_task.go

--- block_parser/block_parser_handle.go
+++ block_parser/block_parser_handle.go
@@ -30,6 +30,7 @@ func (b *BlockParser) registerTransactionHandle() {
 	b.mapTransactionHandle[common.DasActionDeclareReverseRecord] = b.ActionDeclareReverseRecord
 	b.mapTransactionHandle[common.DasActionRedeclareReverseRecord] = b.ActionRedeclareReverseRecord
 	b.mapTransactionHandle[common.DasActionRetractReverseRecord] = b.ActionRetractReverseRecord
+	b.mapTransactionHandle[common.DasActionUpdateReverseRecordRoot] = b.ActionReverseRecordRoot
 }
 
 func isCurrentVersionTx(tx *types.Transaction, name common.DasContractName) (bool, error) {
