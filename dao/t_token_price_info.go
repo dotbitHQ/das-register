47ae27f9c426f8b7d3846c65e0f38c21c9c4b920
scorpiotzh <835598264@qq.com>
2023-06-12 15:39:18 +0800

    feat: Filter payment currencies

--- dao/t_token_price_info.go
+++ dao/t_token_price_info.go
@@ -3,6 +3,6 @@ package dao
 import "das_register_server/tables"
 
 func (d *DbDao) GetTokenPriceList() (list []tables.TableTokenPriceInfo, err error) {
-	err = d.parserDb.Where("token_id NOT IN('bsc_bep20_usdt','eth_erc20_usdt')").Order("id DESC").Find(&list).Error
+	err = d.parserDb.Where("token_id NOT IN('bsc_bep20_usdt','eth_erc20_usdt','tron_trc20_usdt','stripe_usd')").Order("id DESC").Find(&list).Error
 	return
 }
