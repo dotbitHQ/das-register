d2b34a233c8acd1921e488184c4e44f4a5ee22ef
scorpiotzh <835598264@qq.com>
2024-04-26 16:48:09 +0800

    feat: add api for did cell list

--- dao/t_did_cell_info.go
+++ dao/t_did_cell_info.go
@@ -1,15 +1,20 @@
 package dao
 
-import "das_register_server/tables"
+import (
+	"das_register_server/tables"
+	"time"
+)
 
 func (d *DbDao) GetDidAccountList(args string, limit, offset int) (list []tables.TableDidCellInfo, err error) {
-	err = d.parserDb.Where("args=?", args).
+	expiredAt := time.Now().Unix() - 90*86400
+	err = d.parserDb.Where("args=? AND expired_at>=?", args, expiredAt).
 		Limit(limit).Offset(offset).Find(&list).Error
 	return
 }
 
 func (d *DbDao) GetDidAccountListTotal(args string) (count int64, err error) {
+	expiredAt := time.Now().Unix() - 90*86400
 	err = d.db.Model(tables.TableDidCellInfo{}).
-		Where("args=?", args).Count(&count).Error
+		Where("args=? AND expired_at>=?", args, expiredAt).Count(&count).Error
 	return
 }
