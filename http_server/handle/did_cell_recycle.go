6d9a12f8f7aa87fff5b5fc084eb238bc335d1157
scorpiotzh <835598264@qq.com>
2024-07-08 11:19:04 +0800

    feat: update lib

--- http_server/handle/did_cell_recycle.go
+++ http_server/handle/did_cell_recycle.go
@@ -89,11 +89,30 @@ func (h *HttpHandle) doDidCellRecycle(req *ReqDidCellRecycle, apiResp *http_api.
 		return nil
 	}
 
+	// expireAt
+	timeCell, err := h.dasCore.GetTimeCell()
+	if err != nil {
+		apiResp.ApiRespErr(http_api.ApiCodeError500, "failed to get time cell")
+		return fmt.Errorf("GetTimeCell err: %s", err.Error())
+	}
+	builderConfigCell, err := h.dasCore.ConfigCellDataBuilderByTypeArgs(common.ConfigCellTypeArgsAccount)
+	if err != nil {
+		apiResp.ApiRespErr(http_api.ApiCodeError500, "failed to get config cell")
+		return fmt.Errorf("ConfigCellDataBuilderByTypeArgs err: %s", err.Error())
+	}
+	expirationGracePeriod, err := builderConfigCell.ExpirationGracePeriod()
+	if err != nil {
+		apiResp.ApiRespErr(http_api.ApiCodeError500, "ExpirationGracePeriod err")
+		return fmt.Errorf("ExpirationGracePeriod err: %s", err.Error())
+	}
+	expiredAt := uint64(timeCell.Timestamp()) - uint64(expirationGracePeriod)
+	log.Info("doDidCellRecycle:", expiredAt, timeCell.Timestamp(), expirationGracePeriod)
+
 	//expiredAt := tables.GetDidCellRecycleExpiredAt()
-	//if didAccount.ExpiredAt > expiredAt {
-	//	apiResp.ApiRespErr(http_api.ApiCodeNotYetDueForRecycle, "not yet due for recycle")
-	//	return nil
-	//}
+	if didAccount.ExpiredAt > expiredAt {
+		apiResp.ApiRespErr(http_api.ApiCodeNotYetDueForRecycle, "not yet due for recycle")
+		return nil
+	}
 
 	didCellOutpoint := common.String2OutPointStruct(didAccount.Outpoint)
 	txParams, err := txbuilder.BuildDidCellTx(txbuilder.DidCellTxParams{
