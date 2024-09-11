bf9a547ea74738c8f87180a7e5b7a78d0da61089
michael <457813723@qq.com>
2024-09-11 14:43:52 +0800

    test jenkins v1

--- http_server/router.go
+++ http_server/router.go
@@ -101,7 +101,9 @@ func (h *HttpServer) initRouter() {
 
 		// node rpc
 		v1.POST("/node/ckb/rpc", api_code.DoMonitorLog(api_code.MethodCkbRpc), h.h.CkbRpc)
-
+		v1.GET("/test/jenkins", func(c *gin.Context) {
+			c.JSON(200, "main--v1")
+		})
 	}
 
 	internalV1 := h.internalEngine.Group("v1")
