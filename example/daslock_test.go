512e1834357f5b6b3cb2b5a1e25fc16fbafa305a
scorpiotzh <835598264@qq.com>
2024-11-29 09:59:22 +0800

    feat: test daslock dob

--- example/daslock_test.go
+++ example/daslock_test.go
@@ -15,11 +15,12 @@ func TestDidCellDasLockEditOwner(t *testing.T) {
 			Type: "blockchain",
 			KeyInfo: core.KeyInfo{
 				CoinType: common.CoinTypeCKB,
-				Key:      "ckt1qqexmutxu0c2jq9q4msy8cc6fh4q7q02xvr7dc347zw3ks3qka0m6qgrzk3ntzys3nuwmvnar2lrs54l9pat6wy3qv26xdvgjzx03mdj05dtuwzjhu5840fcjyg4hdja",
-				//Key:      "ckt1qqexmutxu0c2jq9q4msy8cc6fh4q7q02xvr7dc347zw3ks3qka0m6qggq7ljq22xat77lgqumuvm685cytdl54du4qyq00eq99rw4l005qwd7xdar6vz9kl62k72s2gs3zc",
+				//Key:      "ckt1qqexmutxu0c2jq9q4msy8cc6fh4q7q02xvr7dc347zw3ks3qka0m6qgrzk3ntzys3nuwmvnar2lrs54l9pat6wy3qv26xdvgjzx03mdj05dtuwzjhu5840fcjyg4hdja",
+				Key: "ckt1qqexmutxu0c2jq9q4msy8cc6fh4q7q02xvr7dc347zw3ks3qka0m6qg9zk3ntzys3nuwmvnar2lrs54l9pat6wy3q526xdvgjzx03mdj05dtuwzjhu5840fcjy2c9u8d",
 			},
 		},
-		Account: "2023100802.bit",
+		//Account: "2023100802.bit",
+		Account: "20240620.bit",
 		RawParam: struct {
 			ReceiverCoinType common.CoinType `json:"receiver_coin_type"`
 			ReceiverAddress  string          `json:"receiver_address"`
@@ -29,27 +30,27 @@ func TestDidCellDasLockEditOwner(t *testing.T) {
 		},
 	}
 
-	addrHexFrom, err := req.FormatChainTypeAddress(common.DasNetTypeTestnet2, true)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Println(addrHexFrom.AddressHex, addrHexFrom.DasAlgorithmId, addrHexFrom.AddressPayload)
+	//addrHexFrom, err := req.FormatChainTypeAddress(common.DasNetTypeTestnet2, true)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//fmt.Println(addrHexFrom.AddressHex, addrHexFrom.DasAlgorithmId, addrHexFrom.AddressPayload)
 
 	url := TestUrl + "/did/cell/daslock/edit/owner"
 	var data handle.RespDidCellDasLockEditOwner
 	if err := doReq(url, req, &data); err != nil {
 		t.Fatal(err)
 	}
+	//fmt.Println(toolib.JsonString(&data))
+	fmt.Println("===========================")
+	//fmt.Println(data.CKBTx)
+
+	if err := doSig(&data.SignInfo); err != nil {
+		t.Fatal(err)
+	}
 	fmt.Println(toolib.JsonString(&data))
 	fmt.Println("===========================")
-	fmt.Println(data.CKBTx)
-
-	//if err := doSig(&data.SignInfo); err != nil {
-	//	t.Fatal(err)
-	//}
-	//fmt.Println(toolib.JsonString(&data))
-	//fmt.Println("===========================")
-	//if err := sendTx2(data.SignInfo); err != nil {
-	//	t.Fatal(err)
-	//}
+	if err := sendTx2(data.SignInfo); err != nil {
+		t.Fatal(err)
+	}
 }
